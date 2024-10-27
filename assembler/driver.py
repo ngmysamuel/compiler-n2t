c971c1898b07443d59fb823d8eaee98e442f99c3
ngmysamuel <e0148675@u.nus.edu>
2024-10-27 14:47:35 +0800

    linted assembler

--- assembler/driver.py
+++ assembler/driver.py
@@ -1,25 +1,40 @@
-import assembler.parser as parser
-import assembler.code as code
-import assembler.symboltable as symboltable
-
 import sys
 
-def assemble(src, dest):
-  print("Initializing classes")
-  c = code.Code()
-  s = symboltable.SymbolTable()
-  with parser.Parser(src, s) as p:
-    while p.advance_first_pass(): # first pass
-      pass
-  with parser.Parser(src, s) as p, open(dest, 'w') as dest_file:
-    while p.advance_second_pass(): # second pass
-      ans = c.map(p)
-      if ans:
-        dest_file.write(ans + "\n")
+from assembler import code, parser, symboltable
 
-def main(src, dest):
-  print("Assembler staring from Command Line")
-  assemble(src, dest)
+
+def assemble(src: str, dest: str) -> None:
+    """Driver function.
+
+    Args:
+        src: Input assembly file to assemble into binary
+        dest: Output binary file
+
+    """
+    print("Initializing classes")
+    c = code.Code()
+    s = symboltable.SymbolTable()
+    with parser.Parser(src, s) as p:
+        while p.advance_first_pass():  # first pass
+            pass
+    with parser.Parser(src, s) as p, open(dest, "w") as dest_file:
+        while p.advance_second_pass():  # second pass
+            ans = c.map(p)
+            if ans:
+                dest_file.write(ans + "\n")
+
+
+def main(src: str, dest: str) -> None:
+    """Entry function.
+
+    Args:
+        src: Input assembly file to assemble into binary
+        dest: Output binary file
+
+    """
+    print("Assembler staring from Command Line")
+    assemble(src, dest)
+
 
 if __name__ == "__main__":
-  main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2])
--- assembler/parser.py
+++ assembler/parser.py
@@ -1,112 +1,149 @@
+"""Parses assembly code into component parts."""
+
+from assembler import symboltable
+
+
 class Parser:
+    __slots__ = (
+        "path",
+        "src_file",
+        "current_line",
+        "instr_type",
+        "sym",
+        "dest",
+        "comp",
+        "jmp",
+        "instruction_counter",
+        "memory_counter",
+        "symbolTable",
+    )
 
-  __slots__ = ("path", "src_file", "current_line", "instr_type", "sym", "dest", "comp", "jmp", "instruction_counter", "memory_counter", "symbolTable")
+    def __init__(self, path: str, symboltable: symboltable.SymbolTable) -> None:
+        if ".asm" not in path:
+            msg = f"Please load a file with extension .asm - attempted to load {path}"
+            raise ValueError(msg)
+        self.path = path
+        self.instruction_counter = 0
+        self.memory_counter = 16
+        self.symbolTable = symboltable
+        self.reset()
 
-  def __init__(self, path, symbolTable):
-    if ".asm" not in path:
-      raise ValueError(f"Please load a file with extension .asm - attempted to load {path}")
-    self.path = path
-    self.instruction_counter = 0
-    self.memory_counter = 16
-    self.symbolTable = symbolTable
-    self.reset()
+    def reset(self) -> None:
+        self.instr_type = None
+        self.sym = None
+        self.dest = None
+        self.comp = None
+        self.jmp = None
 
-  def reset(self):
-    self.instr_type = None
-    self.sym = None
-    self.dest = None
-    self.comp = None
-    self.jmp = None
+    def __enter__(self) -> "Parser":
+        """When creating parser using "with" method, sets src_file."""
+        self.src_file = open(self.path)
+        return self
 
-  def __enter__(self):
-    self.src_file = open(self.path, "r")
-    return self
+    def __exit__(self, exc_type, exc_value, tb) -> bool:
+        """On exit of the "with" method."""
+        if exc_type is not None:
+            print(exc_type, exc_value, tb)
+        self.path = None
+        self.src_file.close()
+        return True
 
-  def __exit__(self, exc_type, exc_value, tb):
-    if exc_type is not None:
-       print(exc_type, exc_value, tb)
-    self.path = None
-    self.src_file.close()
-    return True
-  
-  def clean_lines(self, line):
-    """Sets the cleaned line into self.current_line. And returns True if its the end of the file, else, False"""
-    if not line: # EOF
-      self.current_line = ""
-      return True
+    def clean_lines(self, line: str) -> bool:
+        """Clean up assembly code.
 
-    line = line.strip() # Remove trailing and leading white spaces
+        Set the cleaned line into self.current_line.
+        And returns True if its the end of the file, else, False.
 
-    if len(line) == 0: # Empty line
-      self.current_line = ""
-      return False
+        Args:
+          line: the assembly code to be cleaned
 
-    if "//" in line: # Comments
-      position = line.index("//")
-      if position == 0:
-        self.current_line = ""
-        return False # Entire line is a comment
-      else:
-        line = line[:position].strip()
-    self.current_line = line
-    return False
+        """
+        if not line:  # EOF
+            self.current_line = ""
+            return True
 
-  def advance_first_pass(self):
-    current_line = self.src_file.readline()
+        line = line.strip()  # Remove trailing and leading white spaces
 
-    is_eof = self.clean_lines(current_line)
-    if self.current_line == "":
-      return not is_eof
+        if len(line) == 0:  # Empty line
+            self.current_line = ""
+            return False
 
-    if "(" == self.current_line[0]: # Label
-      self.sym = self.current_line[1:-1]
-      self.symbolTable.put(self.sym, str(self.instruction_counter))
-    else: # A and C instructions
-      self.instruction_counter += 1
+        if "//" in line:  # Comments
+            position = line.index("//")
+            if position == 0:
+                self.current_line = ""
+                return False  # Entire line is a comment
+            line = line[:position].strip()
+        self.current_line = line
+        return False
 
-    return not is_eof
+    def advance_first_pass(self) -> bool:
+        """Set up Symbol Table with the necessary instruction locations.
 
-  def advance_second_pass(self):
-    current_line = self.src_file.readline()
+        Labels can be forward referenced, this first pass ensures that we will be able
+        to catch all these forward referenced labels and still be able to assign the
+        location they need to jump to.
+        Makes use of the src_file we set on __enter__.
+        """
+        current_line = self.src_file.readline()
 
-    is_eof = self.clean_lines(current_line)
-    if self.current_line == "":
-      self.reset()
-      return not is_eof
+        is_eof = self.clean_lines(current_line)
+        if self.current_line == "":
+            return not is_eof
 
-    self.reset()
+        if self.current_line[0] == "(":  # Label
+            self.sym = self.current_line[1:-1]
+            self.symbolTable.put(self.sym, str(self.instruction_counter))
+        else:  # A and C instructions
+            self.instruction_counter += 1
 
-    if "(" == self.current_line[0]: # Label
-      return True
+        return not is_eof
 
-    elif "@" == self.current_line[0]: # A instruction
-      self.instr_type = "A"
-      self.sym = self.current_line[1:]
-      if not self.sym.isnumeric():
-        if self.sym not in self.symbolTable:
-          self.symbolTable.put(self.sym, str(self.memory_counter))
-          self.memory_counter += 1
-        self.sym = self.symbolTable[self.sym]
+    def advance_second_pass(self) -> bool:
+        """Initialize Parser.
 
-    else:
-      self.instr_type = "C" # C instruction
-      if "=" in self.current_line and ";" in self.current_line:
-        tmp = self.current_line.split("=")
-        self.dest = tmp[0].strip()
-        tmp = tmp[1].split(";")
-        self.comp = tmp[0].strip()
-        self.jmp = tmp[1].strip()
-      elif "=" in self.current_line:
-        tmp = self.current_line.split("=")
-        self.dest = tmp[0].strip()
-        self.comp = tmp[1].strip()
-      elif ";" in self.current_line:
-        tmp = self.current_line.split(";")
-        self.comp = tmp[0].strip()
-        self.jmp = tmp[1].strip()
-      else:
-        self.comp = self.current_line.strip()
+        Happens by parsing the asssembly code into different parts such as instruction
+        type, destination, etc and setting that infomation into Parser's attributes.
+        Makes use of the src_file we set on __enter__.
+        """
+        current_line = self.src_file.readline()
 
-    return not is_eof
+        is_eof = self.clean_lines(current_line)
+        if self.current_line == "":
+            self.reset()
+            return not is_eof
 
+        self.reset()
 
+        if self.current_line[0] == "(":  # Label
+            return True
+
+        if self.current_line[0] == "@":  # A instruction
+            self.instr_type = "A"
+            self.sym = self.current_line[1:]
+            if not self.sym.isnumeric():
+                if self.sym not in self.symbolTable:
+                    self.symbolTable.put(self.sym, str(self.memory_counter))
+                    self.memory_counter += 1
+                self.sym = self.symbolTable[self.sym]
+
+        else:
+            self.instr_type = "C"  # C instruction
+            if "=" in self.current_line and ";" in self.current_line:
+                tmp = self.current_line.split("=")
+                self.dest = tmp[0].strip()
+                tmp = tmp[1].split(";")
+                self.comp = tmp[0].strip()
+                self.jmp = tmp[1].strip()
+            elif "=" in self.current_line:
+                tmp = self.current_line.split("=")
+                self.dest = tmp[0].strip()
+                self.comp = tmp[1].strip()
+            elif ";" in self.current_line:
+                tmp = self.current_line.split(";")
+                self.comp = tmp[0].strip()
+                self.jmp = tmp[1].strip()
+            else:
+                self.comp = self.current_line.strip()
+
+        return not is_eof
