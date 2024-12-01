2d1f8df21459c876a2ec1b03fc3ca981462c00a8
ngmysamuel <e0148675@u.nus.edu>
2024-12-01 11:01:50 +0800

    bring files into compiler module

---
+++ compiler/enumerations/TokenType.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class TokenType(Enum):
+  SYMBOL = "symbol"
+  KEYWORD = "keyword"
+  IDENTIFIER = "identifier"
+  INT_CONST = "integerConstant"
+  STRING_CONST = "stringConstant"
