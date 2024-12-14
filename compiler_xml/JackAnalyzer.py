bef6438a62277e074a0377dab6d5c0bcac005e26
ngmysamuel <e0148675@u.nus.edu>
2024-12-14 22:41:53 +0800

    update testing class, compiler_xml to handle directories

--- compiler_xml/JackAnalyzer.py
+++ compiler_xml/JackAnalyzer.py
@@ -1,5 +1,7 @@
-import sys
+import argparse
 from compiler_xml import init_logging as il
+from compiler_xml.init_logging import logger
+import os
 
 from compiler_xml.jack_tokenizer import JackTokenizer
 from compiler_xml.compilation_engine import CompilationEngine
@@ -7,7 +9,37 @@ from compiler_xml.enumerations.SubRoutineType import SubRoutineType
 
 def main(path, log_level, dest_path):
     print(f"path: {path}\nlog_level: {log_level}\ndest_path: {dest_path}")
+    path = "." if path is None else path
+    log_level = "d" if log_level is None else log_level
     il.set_level(log_level)
+
+    if os.path.isfile(path):
+        logger.debug(f"Input file is {path}")
+        if ".jack" not in path:
+            raise ValueError("Input file path needs to be a JACK file")
+        if dest_path is not None and os.path.isdir(dest_path):
+            dest_path = os.path.join(dest_path, str(os.path.basename(path)).replace(".jack", ".xml"))
+        elif ".xml" not in dest_path:
+            raise ValueError("Destination file path needs to be a XML file")
+        elif dest_path is None:
+            dest_path = os.path.join(os.path.dirname(path), str(os.path.basename(path)).replace(".jack", ".xml"))
+        logger.debug(f"dest_path is: {dest_path}")
+        run(path, dest_path)
+    else:
+        logger.debug(f"Input directory is {path}")
+        if dest_path is not None and not os.path.isdir(dest_path):
+            raise ValueError(f"Destination path ({dest_path}) needs to be a directory as input path is a directory ({path})")
+        elif dest_path is None:
+            dest_path = os.path.abspath(path)
+        logger.debug(f"dest path is: {dest_path}")
+        files_and_dirs = os.listdir(path)
+        files_to_process = [f for f in files_and_dirs if f[-5:] == ".jack"]
+        logger.debug(f"Files to process: {files_to_process}")
+        for p in files_to_process:
+            run(os.path.join(path, p), os.path.join(dest_path, p.replace(".jack", ".xml")))
+
+def run(path, dest_path):
+    print(f"path: {path}, dest_path: {dest_path}")
     with CompilationEngine(dest_path) as ce, JackTokenizer(path) as jt:
         try:
             ce.set_tokenizer(jt)
@@ -16,14 +48,9 @@ def main(path, log_level, dest_path):
         ce.compile()
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
-
-
-# py JackAnalyzer.py C:\Users\samue\Documents\Nand2Tetris\06-08_10-11\10\test\test.jack i .\output.xml
-# cd C:\Users\samue\Documents\Nand2Tetris\06-08_10-11\compiler
-# py JackAnalyzer.py C:\Users\samue\Documents\Nand2Tetris\06-08_10-11\10\Square\Main.jack d .\output.xml
-# py JackAnalyzer.py C:\Users\samue\Documents\Nand2Tetris\06-08_10-11\10\ArrayTest\Main.jack i .\output.xml
-# py JackAnalyzer.py C:\Users\samue\Documents\Nand2Tetris\06-08_10-11\10\ExpressionLessSquare\Main.jack i .\output.xml
-
-# py -m compiler.JackAnalyzer C:\Users\samue\Documents\Nand2Tetris\06-08_10-11\10\ExpressionLessSquare\Main.jack i .\output.xml
-# py -m unittest test.compiler.test_intgn
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", help="Path to VM file to compile")
+    parser.add_argument("-l", "--log_level", help="Logging level")
+    parser.add_argument("-o", "--output", help="Path to output file")
+    args = parser.parse_args()
+    main(args.path, args.log_level, args.output)
--- test/compiler_xml/test_intgn.py
+++ test/compiler_xml/test_intgn.py
@@ -9,49 +9,37 @@ class TestIntgration(unittest.TestCase):
 
   file_list = []
 
-  def setUpFiles(self, src_file_path, dest_file_ans_path):
-    src_file = NamedTemporaryFile(
+  def setUpFiles(self, answer_path):
+    answer_file = NamedTemporaryFile(
       delete=False,
       mode="w",
       newline="",
       suffix=".xml",
     )
-    src_file_name = src_file.name
-    with open(src_file_path, "r") as f:
-      src_file.write(f.read())
-    src_file.close()
-
-    dest_file_ans = NamedTemporaryFile(
-      delete=False,
-      mode="w",
-      newline="",
-      suffix=".xml",
-    )
-    dest_file_ans_name = dest_file_ans.name
-    with open(dest_file_ans_path, "r") as f:
+    answer_file_name = answer_file.name
+    with open(answer_path, "r") as f:
       contents = f.read()
       no_white_space = re.sub(r"\s+", "", contents)
-      dest_file_ans.write(no_white_space)
-    dest_file_ans.close()
+      answer_file.write(no_white_space)
+    answer_file.close()
 
-    dest_file_name = dest_file_ans_name.replace(".xml", "2.xml")
+    output_name = answer_file_name.replace(".xml", "2.xml")
 
-    files = (src_file_name, dest_file_ans_name, dest_file_name)
+    files = (answer_file_name, output_name)
     self.file_list.append(files)
     return files
 
   @classmethod
   def tearDownClass(cls):
-      for i, j, k in cls.file_list:
+      for i, j in cls.file_list:
         os.remove(i)
         os.remove(j)
-        os.remove(k)
 
   def test_integration(self):
     files_to_test = [
       (
-        "./10/ExpressionLessSquare/Main.jack",
-        "./10/ExpressionLessSquare/Main.xml"
+          "./10/ExpressionLessSquare/Main.jack",
+          "./10/ExpressionLessSquare/Main.xml"
       ),
       (
         "./10/ExpressionLessSquare/Square.jack",
@@ -78,17 +66,16 @@ class TestIntgration(unittest.TestCase):
         "./10/Square/SquareGame.xml"
       )
       ]
-    for src_file_path, dest_file_ans_path in files_to_test:
-      with self.subTest(src_file_path=src_file_path, dest_file_ans_path=dest_file_ans_path):
-        src_file_name, dest_file_ans_name, dest_file_name = self.setUpFiles(src_file_path, dest_file_ans_path)
-        ja.main(src_file_name, "d", dest_file_name)
-        with open(dest_file_name, "w+") as dest_file, open(dest_file_ans_name, "w+") as dest_file_ans:
-          contents = dest_file.read()
-          no_white_space = re.sub(r"\s+", "", contents)
-          dest_file.write(no_white_space)
-          contents = dest_file_ans.read()
-          no_white_space = re.sub(r"\s+", "", contents)
-          dest_file_ans.write(no_white_space)
-        self.assertTrue(filecmp.cmp(dest_file_name, dest_file_ans_name), "The files are not the same")
-
-
+    for src_path, answer_path in files_to_test:
+      with self.subTest(src_path=src_path, answer_path=answer_path):
+        output_path, answer_path = self.setUpFiles(answer_path)
+        ja.main(src_path, "d", answer_path)
+        with open(answer_path, "r") as answer, open(output_path, "r") as output:
+          contents = answer.read()
+          cleaned_ans = re.sub(r"\s+", "", contents)
+          contents = output.read()
+          cleaned_output = re.sub(r"\s+", "", contents)
+        with open(answer_path, "w") as answer, open(output_path, "w") as output:
+          answer.write(cleaned_ans)
+          output.write(cleaned_output)
+        self.assertTrue(filecmp.cmp(answer_path, output_path), "The files are not the same")
